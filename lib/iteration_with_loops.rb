57f12196a01194bdc97f5c56ad3baeb8e3a0a02d
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-30 17:38:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,5 +4,8 @@ def find_even_values(src)
   # Output all even values in each nested array
   for i in 0...src.size
     for y in 0...src[i].size
+      if src[i][y] % 2 == 0
+        puts src[i][y]
+    end
   end
 end
